253e0a33b85db883ddb2914a809337d5a93df26f
kritika rathi <kritika.rathi@apollomunichinsurance.com>
2019-12-11 16:16:30 +0530

    landscape implementation done

--- src/app/landscape/landscape.component.ts
+++ src/app/landscape/landscape.component.ts
@@ -9,75 +9,73 @@ import { element } from 'protractor';
   styleUrls: ['./landscape.component.scss']
 })
 export class LandscapeComponent implements OnInit {
-  therapySelected:any;
-  user:any;
-   indication:any;
-   indicationSelected:any;
-   indicationSelectedId:any;
-    landscapeData:any;
-    stage=[
-      {
-        "stage": 'Pre-clinical',
-       ' level':0
-      },
-      {
-        "stage": 'Phase I',
-       ' level':1
-      },
-      {
-        "stage": 'Phase IA',
-       ' level':2
-      },
-      {
-        "stage": 'Phase IB',
-       ' level':3
-      },
-      {
-        "stage": 'Phase I/II',
-        ' level':4
-      },
-      {
-        "stage": 'Phase IIA',
-        ' level':5
-      },
-      {
-        "stage": 'Phase IIB',
-        ' level':6
-      },
-      {
-        "stage": 'Phase III',
-        ' level':7
-      },
-      {
-        "stage": 'Phase IIIA',
-        ' level':8
-      },
-      {
-        "stage": 'Phase IIIB',
-        ' level':9
-      },
-      {
-        "stage": 'Phase II/III',
-        ' level':10
-      },
-      {
-        "stage": 'Pre-Registration',
-        ' level':11
-      },
-      {
-        "stage": 'Marketed',
-        ' level':12
-      },
-      {
-        "stage": 'Phase IV',
-        ' level':13
-      }
-    ]
- developmentStage = [];  
-    constructor(private http: HttpClient ) {
-
-
-   }
+  therapySelected: any;
+  user: any;
+  indication: any;
+  indicationSelected: any;
+  indicationSelectedId: any;
+  landscapeData: any;
+  stage = [
+    {
+      "stage": 'Pre-clinical',
+      ' level': 0
+    },
+    {
+      "stage": 'Phase I',
+      ' level': 1
+    },
+    {
+      "stage": 'Phase IA',
+      ' level': 2
+    },
+    {
+      "stage": 'Phase IB',
+      ' level': 3
+    },
+    {
+      "stage": 'Phase I/II',
+      ' level': 4
+    },
+    {
+      "stage": 'Phase IIA',
+      ' level': 5
+    },
+    {
+      "stage": 'Phase IIB',
+      ' level': 6
+    },
+    {
+      "stage": 'Phase III',
+      ' level': 7
+    },
+    {
+      "stage": 'Phase IIIA',
+      ' level': 8
+    },
+    {
+      "stage": 'Phase IIIB',
+      ' level': 9
+    },
+    {
+      "stage": 'Phase II/III',
+      ' level': 10
+    },
+    {
+      "stage": 'Pre-Registration',
+      ' level': 11
+    },
+    {
+      "stage": 'Marketed',
+      ' level': 12
+    },
+    {
+      "stage": 'Phase IV',
+      ' level': 13
+    }
+  ]
+  developmentStage = [];
+  constructor(private http: HttpClient) {
+  }
 
   ngOnInit() {
 
@@ -89,51 +87,51 @@ export class LandscapeComponent implements OnInit {
 
     this.http.post('https://api.vrinda-tea.com/fetchIndicationByTherapy', data).subscribe((data) => {
       this.indication = data
-      this.indicationSelected =  this.indication[0]._id
+      this.indicationSelected = this.indication[0]._id
       this.filterByPhase();
-      console.log('indication',this.indication)
+      console.log('indication', this.indication)
     })
 
   }
 
-  filterByPhase(){
+  filterByPhase() {
     this.http.get('https://api.vrinda-tea.com/fetchAllLandscapeDataByPhase/' + this.indicationSelected).subscribe((data) => {
-      console.log('data',data)
+      console.log('data', data)
       this.landscapeData = data;
-      this.developmentStage=[];
+      this.developmentStage = [];
       this.landscapeData.forEach(element => {
         this.developmentStage.push(element.developmentStage)
-       
+
       });
       this.developmentStage = [...new Set(this.developmentStage)]
       console.log(this.developmentStage)
 
-let  array1=[];
-this.stage.forEach((data)=>{
-  this.developmentStage.filter((stage)=>{
-    if(data.stage == stage){
-      array1.push(data)
-    }
-  })
-})
-console.log(array1)
-var sortedArray = array1.sort(function(a,b){
-  return a.level >b.level?1:a.level <b.level?-1:0
- })
- console.log(sortedArray);
+      let array1 = [];
+      this.stage.forEach((data) => {
+        this.developmentStage.filter((stage) => {
+          if (data.stage == stage) {
+            array1.push(data)
+          }
+        })
+      })
+      console.log(array1)
+      var sortedArray = array1.sort(function (a, b) {
+        return a.level > b.level ? 1 : a.level < b.level ? -1 : 0
+      })
+      console.log(sortedArray);
 
- this.developmentStage = sortedArray;
- console.log( this.developmentStage)
+      this.developmentStage = sortedArray;
+      console.log(this.developmentStage)
     })
 
   }
 
 
 
-  selectedIndication(indication){
+  selectedIndication(indication) {
     this.indicationSelected = indication;
     this.filterByPhase();
   }
- 
+
 
 }
